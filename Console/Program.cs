925e5581fab7641225c00a06cb5c9fd5eb552a4c
dkwok2 <dkwok2@huskers.unl.edu>
2025-03-28 21:11:07 -0500

    Fixed intentional bug/error.

--- Console/Program.cs
+++ Console/Program.cs
@@ -69,9 +69,8 @@ public class Program
 	}
 
 	public static double Subtract(string x, string y)
-		//Intentional error
 	{
-		return double.Parse(x) + double.Parse(y);
+		return double.Parse(x) - double.Parse(y);
 	}
 
 	public static double Multiply(string x, string y)
